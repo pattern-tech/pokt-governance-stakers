eaa3c9dc482cf7c417cb20bceb19095aa9b5c860
satiparpar <satiparpar.com@gmail.com>
2024-01-23 10:17:55 +0330

    write unit test for pokt.retriever ->  request method

--- src/poktscan/tests/pokt.retriever.spec.ts
+++ src/poktscan/tests/pokt.retriever.spec.ts
@@ -3,6 +3,8 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { WinstonProvider } from '@common/winston/winston.provider';
 import { PoktScanRetriever } from '../pokt.retriever';
+import { of } from 'rxjs';
+import { AxiosResponse } from 'axios';
 
 jest.mock('@common/winston/winston.provider');
 
@@ -22,8 +24,68 @@ describe('PoktScanRetriever', () => {
     axios = module.get<HttpService>(HttpService);
     config = module.get<ConfigService>(ConfigService);
     retriever = module.get<PoktScanRetriever>(PoktScanRetriever);
+
+    jest.clearAllMocks();
   });
   test('Should be defined', () => {
     expect(retriever).toBeDefined();
+  });
+
+  describe('request', () => {
+    let query: string;
+    let axiosResponse: AxiosResponse;
+    let variables: Record<string, any>;
+    let returnValue: Record<string, any>;
+
+    beforeEach(async () => {
+      query = 'query { test { test } }';
+      variables = {};
+      axiosResponse = {
+        data: {},
+        status: 200,
+        statusText: 'OK',
+        headers: undefined,
+        config: undefined,
+      };
+      jest.spyOn(config, 'get').mockReturnValue('');
+      jest.spyOn(axios, 'post').mockReturnValue(of(axiosResponse));
+
+      returnValue = await retriever['request'](query, variables);
+    });
+
+    test('Should be defined', () => {
+      expect(retriever['request']).toBeDefined();
+    });
+
+    test('Should call get method from config', () => {
+      // Assert
+      expect(config.get).toHaveBeenCalledWith('POKT_SCAN_API_BASE_URL');
+    });
+
+    test('Should call get method from config', () => {
+      // Assert
+      expect(config.get).toHaveBeenCalledWith('POKT_SCAN_API_TOKEN');
+    });
+
+    test('Should call post from axios with the correct parameters', () => {
+      // Assert
+      expect(axios.post).toHaveBeenCalledWith(
+        '',
+        {
+          query,
+          variables,
+        },
+        {
+          headers: {
+            Authorization: '',
+            'Content-Type': 'application/json',
+          },
+        },
+      );
+    });
+    test('Should return body from http response', () => {
+      // Assert
+      expect(returnValue).toEqual(axiosResponse.data);
+    });
   });
 });
