6620d38d4155b3c2fb1a3dae9b657e38b98a4586
ahmadyazdanii <ahmad.yazdani.pub@gmail.com>
2024-03-09 10:04:14 +0330

    update [wpokt]: remove v3 code blocks

--- src/wpokt/wpokt.service.ts
+++ src/wpokt/wpokt.service.ts
@@ -8,10 +8,6 @@ import {
   WPoktLiquidityV2Response,
   WPoktLiquidityV2Variables,
 } from './interfaces/v2.interface';
-import {
-  WPoktLiquidityV3Variables,
-  WPoktLiquidityV3Response,
-} from './interfaces/v3.interface';
 
 @Injectable()
 export class WPoktService {
@@ -62,14 +58,6 @@ export class WPoktService {
     );
   }
 
-  private async requestV3<T>(query: string, variables?: Record<string, any>) {
-    return this.request<T>(
-      this.config.get<string>('UNISWAP_V3_ENDPOINT_URL'),
-      query,
-      variables,
-    );
-  }
-
   private getUsersWPoktLiquidityV2GQL() {
     return `
     query UsersLiquidity($Users_Wallet_Addr: [String!]!, $WPokt_ID: String!, $Pool_ID: String!) {
@@ -99,25 +87,6 @@ export class WPoktService {
       }`;
   }
 
-  private getUsersWPoktLiquidityV3GQL() {
-    return `
-    query UsersLiquidity($Users_Wallet_Addr: [Bytes!]!, $WPokt_ID: String!) {
-        positions(
-          where: {pool_: {or: [{token0: $WPokt_ID}, {token1: $WPokt_ID}]}, owner_in: $Users_Wallet_Addr}
-        ) {
-          owner
-          token0 {
-            id
-          }
-          depositedToken0
-          token1 {
-            id
-          }
-          depositedToken1
-        }
-    }`;
-  }
-
   private async getUsersWPoktLiquidityV2(usersWalletAddr: Array<string>) {
     const query = this.getUsersWPoktLiquidityV2GQL();
     const variables: WPoktLiquidityV2Variables = {
@@ -127,16 +96,6 @@ export class WPoktService {
     };
 
     return this.requestV2<WPoktLiquidityV2Response>(query, variables);
-  }
-
-  private async getUsersWPoktLiquidityV3(usersWalletAddr: Array<string>) {
-    const query = this.getUsersWPoktLiquidityV3GQL();
-    const variables: WPoktLiquidityV3Variables = {
-      Users_Wallet_Addr: lodash.map(usersWalletAddr, lodash.toLower),
-      WPokt_ID: this.config.get<string>('UNISWAP_WPOKT_TOKEN_ID'),
-    };
-
-    return this.requestV3<WPoktLiquidityV3Response>(query, variables);
   }
 
   private serializeUsersWPoktLiquidityV2(
@@ -168,40 +127,6 @@ export class WPoktService {
     return serializedResponse;
   }
 
-  private serializeUsersWPoktLiquidityV3(
-    response: WPoktLiquidityV3Response,
-  ): Record<string, number> {
-    const serializedResponse = {};
-    const positions = response.data.positions;
-    const WPOKT_TOKEN_ID = this.config.get<string>('UNISWAP_WPOKT_TOKEN_ID');
-
-    for (let index = 0; index < positions.length; index++) {
-      const position = positions[index];
-      const point =
-        position.token0.id === WPOKT_TOKEN_ID
-          ? Number(position.depositedToken0)
-          : Number(position.depositedToken1);
-
-      if (position.owner in serializedResponse) {
-        serializedResponse[position.owner] += point;
-      } else {
-        serializedResponse[position.owner] = point;
-      }
-    }
-
-    return serializedResponse;
-  }
-
-  private combineUsersWPoktLiquidityResponses(
-    v2Response: WPoktLiquidityV2Response,
-    v3Response: WPoktLiquidityV3Response,
-  ): Record<string, number> {
-    const v2Results = this.serializeUsersWPoktLiquidityV2(v2Response);
-    const v3Results = this.serializeUsersWPoktLiquidityV3(v3Response);
-
-    return lodash.mergeWith(v3Results, v2Results, lodash.sum);
-  }
-
   async getUsersWPoktLiquidity(
     GIDsWalletAddresses: Record<string, Array<string>>,
   ) {
@@ -219,15 +144,11 @@ export class WPoktService {
         partitionWalletAddresses.push(...wallets);
       }
 
-      const [partitionV2Response, partitionV3Response] = await Promise.all([
-        this.getUsersWPoktLiquidityV2(partitionWalletAddresses),
-        this.getUsersWPoktLiquidityV3(partitionWalletAddresses),
-      ]);
-
-      const partitionResult = this.combineUsersWPoktLiquidityResponses(
-        partitionV2Response,
-        partitionV3Response,
+      const partitionV2Response = await this.getUsersWPoktLiquidityV2(
+        partitionWalletAddresses,
       );
+      const partitionResult =
+        this.serializeUsersWPoktLiquidityV2(partitionV2Response);
 
       for (let p_idx = 0; p_idx < partitionGatewayIDs.length; p_idx++) {
         const gatewayId = partitionGatewayIDs[p_idx];
