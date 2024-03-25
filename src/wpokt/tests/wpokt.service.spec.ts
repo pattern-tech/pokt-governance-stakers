2671e731ccd8cc6bfe7da1823a51ec430ec9a3b5
satiparpar <mohammad.parpar.99@gmail.com>
2024-03-25 12:23:59 +0330

    add[wpokt.service.spec]: implement unit tests for "getUsersWPoktLiquidityV2GQL" and "getUsersWPoktLiquidityV2" methods

--- src/wpokt/tests/wpokt.service.spec.ts
+++ src/wpokt/tests/wpokt.service.spec.ts
@@ -2,6 +2,7 @@ import { HttpModule, HttpService } from '@nestjs/axios';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AxiosResponse } from 'axios';
+import lodash from 'lodash';
 import { of } from 'rxjs';
 import { WinstonProvider } from '@common/winston/winston.provider';
 import { WPoktService } from '../wpokt.service';
@@ -110,6 +111,76 @@ describe('PDAService', () => {
     test('Should call request method with correct parameters', async () => {
       await wpokt['requestV2'](query, variables);
       expect(wpokt['request']).toHaveBeenCalledWith('', query, variables);
+    });
+  });
+
+  describe('getUsersWPoktLiquidityV2GQL', () => {
+    test('Should be defined', () => {
+      expect(wpokt['getUsersWPoktLiquidityV2GQL']).toBeDefined();
+    });
+    test('Should return getUsersWPoktLiquidityV2 graphQL query', () => {
+      expect(wpokt['getUsersWPoktLiquidityV2GQL']()).toBe(
+        `
+    query UsersLiquidity($Users_Wallet_Addr: [String!]!, $WPokt_ID: String!, $Pool_ID: String!) {
+        positions: liquidityPositions(
+          where: {
+            pair: $Pool_ID,
+            pair_: {or: [{token0: $WPokt_ID}, {token1: $WPokt_ID}]}, 
+            user_in: $Users_Wallet_Addr
+          }
+        ) {
+          user {
+            id
+          }
+          liquidityTokenBalance
+          pair {
+            totalSupply
+            token0 {
+              id
+            }
+            reserve0
+            token1 {
+              id
+            }
+            reserve1
+          }
+        }
+      }`,
+      );
+    });
+  });
+
+  describe('getUsersWPoktLiquidityV2', () => {
+    let usersWalletAddr: Array<string>;
+    beforeEach(() => {
+      usersWalletAddr = [''];
+      jest.spyOn(lodash, 'map').mockReturnValue([true]);
+      jest.spyOn(wpokt as any, 'requestV2').mockReturnValue({});
+      jest
+        .spyOn(wpokt as any, 'getUsersWPoktLiquidityV2GQL')
+        .mockReturnValue('');
+      jest.spyOn(config, 'get').mockReturnValue('');
+    });
+    test('Should be defined', () => {
+      expect(wpokt['getUsersWPoktLiquidityV2']).toBeDefined();
+    });
+    test('Should call getUsersWPoktLiquidityV2GQL method', async () => {
+      await wpokt['getUsersWPoktLiquidityV2'](usersWalletAddr);
+      expect(wpokt['getUsersWPoktLiquidityV2GQL']).toHaveBeenCalled();
+    });
+    test('Should call map method from lodash with correct parameters', async () => {
+      await wpokt['getUsersWPoktLiquidityV2'](usersWalletAddr);
+      expect(lodash.map).toHaveBeenCalledWith(usersWalletAddr, lodash.toLower);
+    });
+    test('Should call get method from config with correct parameter', async () => {
+      await wpokt['getUsersWPoktLiquidityV2'](usersWalletAddr);
+      expect(config.get).toHaveBeenCalledWith('UNISWAP_WPOKT_TOKEN_ID');
+      expect(config.get).toHaveBeenCalledWith('UNISWAP_V2_WPOKT_POOL_ID');
+    });
+    test('Should return value from requestV2 method', async () => {
+      expect(await wpokt['getUsersWPoktLiquidityV2'](usersWalletAddr)).toEqual(
+        {},
+      );
     });
   });
 });
